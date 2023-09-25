4713561de3c402dd3031abc84bd22fff78cc59c4
Akmalzhan <130217051+Akmalzhan01@users.noreply.github.com>
2023-09-25 09:54:50 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
     {
-        origin: ["https://funds-db-api.vercel.app/"],
-        methods: ["POST", "GET"],
-        credentials: true
+        origin: ["*"],
     }
 ))
 
